8f5d6cff4e547bc116087c7e7a2f307f68625b4b
sovandasmaity007 <dasmaity.sovan@gmail.com>
2021-07-24 19:01:35 +0530

    change on add

--- MVVMDemo/Controller/SignUp/SignUpVC.swift
+++ MVVMDemo/Controller/SignUp/SignUpVC.swift
@@ -17,6 +17,7 @@ class SignUpVC: UIViewController {
         // Do any additional setup after loading the view.
         print(self.multipication())
         print(self.add())
+        print(self.add())
     }
 
     func multipication() -> Int {
