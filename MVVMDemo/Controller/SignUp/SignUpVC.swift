d442c01d3a92d420f56689ef7f8c8212e29c3600
sovandasmaity007 <dasmaity.sovan@gmail.com>
2021-07-24 17:49:23 +0530

    Signup VC

---
+++ MVVMDemo/Controller/SignUp/SignUpVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpVC.swift
+//  MVVMDemo
+//
+//  Created by AJAY on 24/07/21.
+//  Copyright © 2021 Individual. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
